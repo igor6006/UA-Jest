3579f8ea54fc9d8230a6a4a50655f5130568a2ae
Castegnaro <matheuscastegnaro@gmail.com>
2023-04-11 20:38:31 -0300

    Teste conta tradicional.

---
+++ src/Conta.js
@@ -0,0 +1,33 @@
+class Conta {
+
+    constructor() {
+        this.saldo = 0;
+        this.ativo = false;
+    }
+
+    getSaldo() {
+        return this.saldo;
+    }
+
+    ativar() {
+        this.ativo = true;
+    }
+
+    inativar() {
+        this.ativo = false;
+    }
+
+    depositar(valor) {
+        if (this.ativo) {
+            this.saldo += valor;
+        }
+    }
+
+    sacar(valor) {
+        if (this.ativo && this.saldo - valor >= 0) {
+            this.saldo -= valor;
+        }
+    }
+}
+
+module.exports = Conta;
