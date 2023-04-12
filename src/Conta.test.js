985e951d823c25e4112abf6296ca6267cd63328d
Castegnaro <matheuscastegnaro@gmail.com>
2023-04-12 19:57:24 -0300

    Melhoria de codico com o beferoAll

--- src/Conta.test.js
+++ src/Conta.test.js
@@ -3,42 +3,54 @@ const Conta = require("./Conta");
 describe("Conta", () => {
     let conta;
 
+    let depositoInicial,
+        saqueValido,
+        saqueInvalido,
+        saldoZero;
+
+    beforeAll(() => {
+        depositoInicial = 100;
+        saqueValido = 70;
+        saqueInvalido = 110.0;
+        saldoZero = 0;
+    })
+
     beforeEach(() => {
         conta = new Conta();
         conta.ativar();
     })
 
     test("Deve depositar com conta ativa", () => {
-        conta.depositar(100.00);
-        expect(conta.getSaldo()).toBe(100);
+        conta.depositar(depositoInicial);
+        expect(conta.getSaldo()).toBe(depositoInicial);
     });
 
     test("Não deve depositar com conta inativa", () => {
         conta.inativar();
-        conta.depositar(100.00);
+        conta.depositar(depositoInicial);
 
-        expect(conta.getSaldo()).toBe(0);
+        expect(conta.getSaldo()).toBe(saldoZero);
     });
 
     test("Deve sacar com conta ativa e saldo maior que o valor de saque", () => {
-        conta.depositar(100.00);
-        conta.sacar(70.0);
+        conta.depositar(depositoInicial);
+        conta.sacar(saqueValido);
 
-        expect(conta.getSaldo()).toBe(30);
+        expect(conta.getSaldo()).toBe(depositoInicial - saqueValido);
     });
 
     test("Não deve sacar com conta inativa", () => {
-        conta.depositar(100.00);
+        conta.depositar(depositoInicial);
         conta.inativar();
-        conta.sacar(70.0);
+        conta.sacar(saqueValido);
 
-        expect(conta.getSaldo()).toBe(100);
+        expect(conta.getSaldo()).toBe(depositoInicial);
     });
 
     test("Não deve sacar com conta ativa e saldo menor que o valor de saque", () => {
-        conta.depositar(100.00);
-        conta.sacar(110.0);
+        conta.depositar(depositoInicial);
+        conta.sacar(saqueInvalido);
 
-        expect(conta.getSaldo()).toBe(100);
+        expect(conta.getSaldo()).toBe(depositoInicial);
     });
 })
