3c1a57ec841a5fa9bf9ee28bd53ff99e6446e442
igor <igoors27@gmail.com>
2023-04-12 15:28:36 -0300

    teste

--- src/Conta.test.js
+++ src/Conta.test.js
@@ -1,8 +1,10 @@
 const Conta = require("./Conta");
+const Cliente = require("./Cliente");
 
 describe("Conta", () => {
     test("deve depositar com conta ativa", () => {
         const conta = new Conta();
+        const cli = new Cliente();
 
         conta.ativar();
         conta.depositar(100.00);
