68bc0024415b88b554530c3b9deec3c9ecb3cab2
igor <igoors27@gmail.com>
2023-04-12 15:34:38 -0300

    teste

--- src/Conta.js
+++ src/Conta.js
@@ -31,3 +31,5 @@ class Conta{
         }
     }
 }
+
+module.exports = Conta;
--- src/Conta.test.js
+++ src/Conta.test.js
@@ -1,10 +1,8 @@
 const Conta = require("./Conta");
-const Cliente = require("./Cliente");
 
 describe("Conta", () => {
     test("deve depositar com conta ativa", () => {
         const conta = new Conta();
-        const cli = new Cliente();
 
         conta.ativar();
         conta.depositar(100.00);
