d8a0a78f613339dd006b66e93980b58e5df94b34
igor <igoors27@gmail.com>
2023-04-12 19:42:56 -0300

    teste

--- src/Conta.test.js
+++ src/Conta.test.js
@@ -9,4 +9,45 @@ describe("Conta", () => {
 
         expect(conta.getSaldo()).toBe(100.00);
     });
+    
+
+    test("Não deve depositar com conta inativa", () => {
+        const conta = new Conta();
+
+        conta.inativar();
+        conta.depositar(100.00);
+
+        expect(conta.getSaldo()).toBe(0);
+    });
+
+    test("Deve sacar com conta ativa e saldo maior que o valor de saque", () => {
+        const conta = new Conta();
+
+        conta.ativar();
+        conta.depositar(100.00);
+        conta.sacar(70.0);
+
+        expect(conta.getSaldo()).toBe(30);
+    });
+
+    test("Não deve sacar com conta inativa", () => {
+        const conta = new Conta();
+
+        conta.ativar();
+        conta.depositar(100.00);
+        conta.inativar();
+        conta.sacar(70.0);
+
+        expect(conta.getSaldo()).toBe(100);
+    });
+
+    test("Não deve sacar com conta ativa e saldo menor que o valor de saque", () => {
+        const conta = new Conta();
+
+        conta.ativar();
+        conta.depositar(100.00);
+        conta.sacar(110.0);
+
+        expect(conta.getSaldo()).toBe(100);
+    });
 })
